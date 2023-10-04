6c2a312ce2227a2919f1bc45fa2634c91bcb141c
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-03 20:07:09 -0600

    Add a FeedbackItem component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
-export default function Header({ text }) {
+export default function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container">
             <h2>{text}</h2>
         </div>
     </header>
   )
 }
+
 Header.defaultProps = {
-    text: 'Feedback UI', 
+    text: 'Feedback UI',
+    bgColor: 'rgba(0, 0, 0, 0.4)',
+    textColor: '#ff6a95',
 }
