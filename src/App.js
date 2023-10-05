d679eb23706601ab4e03b6a9ec906f5d41b7f3f3
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-05 14:07:17 -0600

    Add new Feddback comment trough the form

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+    }
 
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
@@ -18,7 +24,7 @@ export default function App() {
         <>
             <Header text="Hello World" />
             <div className="container">
-                <FeedbackForm />
+                <FeedbackForm handleAdd={addFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList 
                     feedback={feedback}
