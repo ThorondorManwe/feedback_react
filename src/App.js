f4089ab67c169023854d6e23c610b25224d1a7ba
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-05 12:55:34 -0600

    Add a form input and a custom button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 
 export default function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,6 +18,7 @@ export default function App() {
         <>
             <Header text="Hello World" />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList 
                     feedback={feedback}
