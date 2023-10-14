a3f5b652f57e2bf8396df200deed432b7cf8e047
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-14 13:04:55 -0600

    Update and delete from JSON-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -24,9 +24,12 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback(data);
     setIsLoading(false);
   };
-
-  const deleteFeedback = (id) => {
+  
+  // Delete feedback
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' })
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -48,9 +51,19 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-        feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item ))
+        feedback.map((item) => (item.id === id ? { ...item, ...data } : item ))
     )
   }
 
