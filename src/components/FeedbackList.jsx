42f85acaf58bd32a8b83ee45b1832295bbdfbd99
Carlos Rangel <charlesleimaginant@gmail.com>
2023-10-14 12:21:05 -0600

    Add a spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from './shared/Spinner';
 // import PropTypes from "prop-types";
 import { motion, AnimatePresence } from "framer-motion";
 import FeedbackContext from "../context/FeedbackContext";
@@ -7,12 +8,12 @@ import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
 
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
-  return (
+  return isLoading ? <Spinner /> : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
